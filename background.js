653993a23a87b66212371e2d49f1277c2a9e6a05
Sai Bhaskar Devatha <saibhaskardevatha@gmail.com>
2022-10-26 11:21:15 +0530

    Fixed tabs permission error.

--- background.js
+++ background.js
@@ -1,20 +1,5 @@
 chrome.action.onClicked.addListener(function (tab) {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		// chrome.windows.create(
-		// 	{
-		// 		url: tabs[0].url,
-		// 		type: "popup",
-		// 		focused: false,
-		// 		top: 0,
-		// 		left: 0,
-		// 		// width: 360,
-		// 		// height: 630,
-		// 	},
-		// 	(createdWindow) => {
-		// 		// console.log(createdWindow?.id, "Assigning popup to id");
-		// 		sendResponse("continue");
-		// 	}
-		// );
 		chrome.tabs.sendMessage(tabs[0].id, {
 			action: "get_mutual_connections",
 		});
